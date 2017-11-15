c82237b2bb2d2b332cd979f3f0c7afb1ad140b6a
Gabriel Martinez <gabriel.martinez.reyes@gmail.com>
2017-11-15 09:33:19 +0100

    Fade in & Fade Out Objects

--- FadingObjects/FadingObjects/ViewController.swift
+++ FadingObjects/FadingObjects/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var segControl: UISegmentedControl!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var slider: UISlider!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     } 
 
-
+    @IBAction func fadeInBtn(_ sender: Any) {
+        UIView.animate(withDuration: 1.0, animations: {
+            
+            self.label.alpha = 1
+            self.segControl.alpha = 1
+            self.button.alpha = 1
+            
+            
+            
+        })
+    }
+    
+    @IBAction func fadeOutBtn(_ sender: Any) {
+        self.label.alpha = 0
+        self.segControl.alpha = 0
+        self.button.alpha = 0
+    }
+    
+     
+    @IBAction func alphaSlider(_ sender: Any) {
+        label.alpha = CGFloat(slider.value)
+        button.alpha = CGFloat(slider.value)
+        segControl.alpha = CGFloat(slider.value)
+    }
+    
+    
+    
+    
+    
+    
+    
 }
 
